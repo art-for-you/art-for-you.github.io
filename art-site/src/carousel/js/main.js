e7d0496cbaa0a03af76d7dd474cb1ec46de3601c
art-for-you <67659407+art-for-you@users.noreply.github.com>
2020-07-01 03:50:53 +0700

    add files

---
+++ art-site/src/carousel/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		margin: 10,
+		navText: ["<img src='img/arrow-left.png'>", "<img src='img/arrow-right.png'>"],
+		items:3,
+	});
+});
